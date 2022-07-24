89f0883b1d526eb4b677248623b34a377428ff88
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-07-24 15:42:03 +0100

    Select the h2 & button elements, use in-built fetch method to make API data request, and finally display the data on the User Interface

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ 
+// Select html element
+const advice = document.querySelector(".message");
+const btnAdvice = document.querySelector(".btn");
+
+
+// Get data from an api
+const getAdvice = async() => {
+    const api = await fetch("https://api.adviceslip.com/advice");
+    const data = await api.json();
+    advice.textContent = data.slip.advice;
+}
+
+
+// Display data on the UI
+btnAdvice.addEventListener("click", () => {
+    getAdvice();
+});
